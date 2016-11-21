e84976a634686a56a140cadcbf8297c44ca04a03
JonZeolla <Zeolla@gmail.com>
2016-11-21 10:07:26 -0500

    Update ScatterPlot.R

--- Udacity/IntroToStatistics/Visualization/ScatterPlot.R
+++ Udacity/IntroToStatistics/Visualization/ScatterPlot.R
@@ -1,8 +1,24 @@
 
 library(ggplot2)
+
+givemeppsf <- function(price, size) {
+  return(price / size)
+}
+
 thesizes <- c(1400, 2400, 1800, 1900, 1300, 1100)
 theprices <- c(98000, 168000, 126000, 133000, 91000, 77000)
-df <- data.frame(size = thesizes, price = theprices)
-ggplot(df, aes(x=df$size, y=df$price)) +
- geom_point(shape=1) +
- geom_smooth(method=lm)
+dfFirst <- data.frame(size = thesizes, price = theprices)
+ggplot(dfFirst, aes(x=dfFirst$size, y=dfFirst$price)) +
+  geom_point(shape=1) +
+  geom_smooth(method=lm)
+dfFirst$ppsf <- mapply(givemeppsf, dfFirst$price, dfFirst$size)
+dfFirst
+
+thesizes <- c(1700, 2100, 1900, 1300, 1600, 2200)
+theprices <- c(51000, 63000, 57000, 39000, 48000, 66000)
+dfSecond <- data.frame(size = thesizes, price = theprices)
+ggplot(dfSecond, aes(x=dfSecond$size, y=dfSecond$price)) +
+  geom_point(shape=1) +
+  geom_smooth(method=lm)
+dfSecond$ppsf <- mapply(givemeppsf, dfSecond$price, dfSecond$size)
+dfSecond
